ced625179fdec083b3acb2c990908a7bccc6cd49
mik25 <mikmc55@gmail.com>
2023-04-03 19:24:31 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,7 +2,7 @@ module.exports={
     id: "org.community.YourIPTV",
     version: "1.1.1",
     name: "IPTV TEST",
-    logo: "https://2maxtv2.surge.sh/2776.jpg",
+    logo: "https://raw.githubusercontent.com/mik25/stremio-greek-tv/master/hank.jpg",
     description: "Multi Header M3U Tester",
     types: ["movie", "series","tv","channel"],
     background: "https://dl.strem.io/addon-background.jpg",
