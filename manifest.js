4b80e09d021e662db8f2265d8dc87cb292ca2604
mik25 <mikmc55@gmail.com>
2023-09-20 00:58:44 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,9 +1,9 @@
 module.exports={
     id: "org.community.YourIPTV",
     version: "1.1.1",
-    name: "IPTV TEST",
+    name: "IPTV DISCORD",
     logo: "https://cdn.discordapp.com/icons/667359689780101130/596f5a89339ba7c31a873ff3559645bb.webp?size=160",
-    description: "Multi Header M3U Tester",
+    description: "Discord Your-IPTV",
     types: ["movie", "series","tv","channel"],
     background: "https://dl.strem.io/addon-background.jpg",
     resources: ["movie","series","tv"],
