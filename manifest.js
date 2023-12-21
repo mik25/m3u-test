92e97e4d4ef7094ae34583d7fec6a531edbbf054
mik25 <mikmc55@gmail.com>
2023-12-21 14:35:24 +0000

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,9 +1,9 @@
 module.exports={
     id: "org.community.YourIPTV",
     version: "1.1.1",
-    name: "IPTV DISCORD",
-    logo: "https://cdn.discordapp.com/icons/667359689780101130/596f5a89339ba7c31a873ff3559645bb.webp?size=160",
-    description: "Discord Your-IPTV",
+    name: "IPTV HEMS",
+    logo: "https://raw.githubusercontent.com/mik25/hy-uhd/main/self-portrait-my-music-room-ride-of-the-valkyries-thomas-pollart%20(2).jpg",
+    description: "STREMIO IPTV",
     types: ["movie", "series","tv","channel"],
     background: "https://dl.strem.io/addon-background.jpg",
     resources: ["movie","series","tv"],
