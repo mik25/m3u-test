bc0db1b9919871c471c27399f6e019faa21ff70e
mik25 <mikmc55@gmail.com>
2024-01-08 12:48:25 +0000

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,7 +2,7 @@ module.exports={
     id: "org.community.YourIPTV",
     version: "2.0.0",
     name: "MikMc IPTV",
-    logo: "https://raw.githubusercontent.com/mik25/hy-uhd/main/self-portrait-my-music-room-ride-of-the-valkyries-thomas-pollart%20(2).jpg",
+    logo: "https://raw.githubusercontent.com/mik25/free-domains/master/self-portrait-my-music-room-ride-of-the-valkyries-thomas-pollart%20(2).jpg",
     description: "STREMIO IPTV",
     types: ["movie", "series","tv","channel"],
     background: "https://raw.githubusercontent.com/mik25/m3u-test/main/blownaway.jpg",
