33c72adca02c3ca8e2f0c0327fddacc3c60c54d6
mik25 <mikmc55@gmail.com>
2024-02-14 09:36:28 +0000

    Update addon.js

--- addon.js
+++ addon.js
@@ -112,7 +112,7 @@ let live
     }
     const manifest = {
         id:`org.community.${obj.domainName}` || "org.community.youriptv",
-        version:"1.0.0",
+        version:"1.0.1m",
         name:obj.domainName + " IPTV" || "Your IPTV",
         description:`You will access to your ${obj.domainName} IPTV with this addon!`,
         idPrefixes:[obj.idPrefix],
